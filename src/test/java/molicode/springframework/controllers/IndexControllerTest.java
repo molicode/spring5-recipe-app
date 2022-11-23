aa1881e89bd328c3fb1eaf0ed5427212a2bb157d
Luis Molina <molicode1347@gmail.com>
2022-11-23 14:32:03 -0300

    Introducing to Spring MockMVC

--- src/test/java/molicode/springframework/controllers/IndexControllerTest.java
+++ src/test/java/molicode/springframework/controllers/IndexControllerTest.java
@@ -2,9 +2,13 @@ package molicode.springframework.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +20,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 class IndexControllerTest {
@@ -33,6 +40,15 @@ class IndexControllerTest {
     MockitoAnnotations.initMocks(this);
 
     controller = new IndexController(recipeService);
+  }
+
+  @Test
+  void testMockMVC() throws Exception {
+    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+    mockMvc.perform(get("/"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("index"));
   }
 
   @Test
