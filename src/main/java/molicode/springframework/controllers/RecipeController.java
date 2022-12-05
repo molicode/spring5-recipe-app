76097b1fdec66566e19737210f042045f57bdd24
Luis Molina <molicode1347@gmail.com>
2022-12-05 16:35:17 -0300

    delete function

--- src/main/java/molicode/springframework/controllers/RecipeController.java
+++ src/main/java/molicode/springframework/controllers/RecipeController.java
@@ -1,14 +1,17 @@
 package molicode.springframework.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import molicode.springframework.commands.RecipeCommand;
 import molicode.springframework.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -18,6 +21,7 @@ public class RecipeController {
     this.recipeService = recipeService;
   }
 
+  @GetMapping
   @RequestMapping("/recipe/{id}/show")
   public String showById(@PathVariable String id, Model model) {
 
@@ -25,6 +29,7 @@ public class RecipeController {
     return "recipe/show";
   }
 
+  @GetMapping
   @RequestMapping("recipe/new")
   public String newRecipe(Model model) {
     model.addAttribute("recipe", new RecipeCommand());
@@ -32,6 +37,7 @@ public class RecipeController {
     return "recipe/recipeform";
   }
 
+  @GetMapping
   @RequestMapping("recipe/{id}/update")
   public String updateRecipe(@PathVariable String id, Model model) {
     model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -44,4 +50,15 @@ public class RecipeController {
 
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
   }
+
+  @GetMapping
+  @RequestMapping("recipe/{id}/delete")
+  public String deleteById(@PathVariable String id) {
+
+    log.debug("Deleting id: " + id);
+
+    recipeService.deleteById(Long.valueOf(id));
+    return "redirect:/";
+  }
+
 }
