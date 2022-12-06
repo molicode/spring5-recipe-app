7d197cd20d440980943b593a39a7730ebff87fba
Luis Molina <molicode1347@gmail.com>
2022-12-06 16:07:13 -0300

    Enumeration dropdowns with spring mvc and thymeleaf

--- src/main/java/molicode/springframework/controllers/RecipeController.java
+++ src/main/java/molicode/springframework/controllers/RecipeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
@@ -21,24 +20,21 @@ public class RecipeController {
     this.recipeService = recipeService;
   }
 
-  @GetMapping
-  @RequestMapping("/recipe/{id}/show")
+  @GetMapping("/recipe/{id}/show")
   public String showById(@PathVariable String id, Model model) {
 
     model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
     return "recipe/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/new")
+  @GetMapping("recipe/new")
   public String newRecipe(Model model) {
     model.addAttribute("recipe", new RecipeCommand());
 
     return "recipe/recipeform";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{id}/update")
+  @GetMapping("recipe/{id}/update")
   public String updateRecipe(@PathVariable String id, Model model) {
     model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
     return "recipe/recipeform";
@@ -51,8 +47,7 @@ public class RecipeController {
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{id}/delete")
+  @GetMapping("recipe/{id}/delete")
   public String deleteById(@PathVariable String id) {
 
     log.debug("Deleting id: " + id);
@@ -60,5 +55,4 @@ public class RecipeController {
     recipeService.deleteById(Long.valueOf(id));
     return "redirect:/";
   }
-
 }
